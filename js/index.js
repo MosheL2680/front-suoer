c06cba2bc3f8880de100dde36b2b46d0fc41caec
MosheL2680 <moshelubetski@gmail.com>
2023-11-08 13:55:22 +0200

    5

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ const loadProducts = async () => {
   displayProducts()
 }
 
-// Display products
+// Display products 
 const displayProducts = async () => {
   displayy.innerHTML =
     displayy.innerHTML = `<div class="row row-cols-1 row-cols-md-6 g-1">` + products.map((prod, ind) => `
