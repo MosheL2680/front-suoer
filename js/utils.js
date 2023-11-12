3560824964e691a1bbd1536d13684f2e792df16d
MosheL2680 <moshelubetski@gmail.com>
2023-11-12 11:59:53 +0200

    morning

--- js/utils.js
+++ js/utils.js
@@ -114,7 +114,8 @@ const OrderHistoryLink = () => {
 const displaySpiner = () => {
   return `<div class="spinner-grow" role="status">
   <span class="visually-hidden">Loading...</span>
-</div>`
+</div>
+`
 }
 
 // toggle dark mode
